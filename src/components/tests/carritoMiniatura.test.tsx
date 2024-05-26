f3e9f3d6e6ec150942b927e3140b4617481c2f0e
matiashernandezkopi <cafetrucado@gmail.com>
2024-05-26 17:49:20 -0300

    test acomodado

--- src/components/tests/carritoMiniatura.test.tsx
+++ src/components/tests/carritoMiniatura.test.tsx
@@ -9,7 +9,7 @@ import { CarritoMiniatura } from '../carritoMinuatura';
 
 
 describe('CarritoMiniatura', () => {
-
+    
     const preloadedState = {
         carritoArray: [
           { id: '1', name: 'Prenda 1', img: 'img1.png', price: 100 },
@@ -24,48 +24,29 @@ describe('CarritoMiniatura', () => {
         preloadedState,
     });
 
-    
-
-  test('Expand button can expand the carrito, taco taco', () => {
-    // Prepopulate the store with some data for testing
-    
-
-    render(
-      <Provider store={testStore}>
-        <CarritoMiniatura carritoArray={preloadedState.carritoArray} />
-      </Provider>
-    );
-
-    
-    const expandButton = screen.getByRole('button', { name: /Carrito de compra/i });
-    
-    fireEvent.click(expandButton);
-    
-
-    // Now check for the presence of the "Total" text
-    expect(screen.queryByText(/Total:/i)).toBeDefined();
-  });
+    beforeEach(()=>{
+        render(
+            <Provider store={testStore}>
+              <CarritoMiniatura carritoArray={preloadedState.carritoArray} />
+            </Provider>
+          );
+    })
 
 
-  
-
-  test('Expand button can close the carrito as well, quesadillas wey', () => {
-    // Prepopulate the store with some data for testing
-   
-
-    render(
-      <Provider store={testStore}>
-        <CarritoMiniatura carritoArray={preloadedState.carritoArray} />
-      </Provider>
-    );
-    const expandButton = screen.getByRole('button', { name: /Carrito de compra/i });
-    
-    
-    fireEvent.click(expandButton);
-    fireEvent.click(expandButton);
-    
 
     
-    expect(screen.queryByText(/Total:/i)).toBeNull();
-  });
+    test('Expand button can expand the carrito, taco taco', () => {
+        const expandButton = screen.getByRole('button', { name: /Carrito de compra/i });
+        fireEvent.click(expandButton);
+        
+        expect(screen.queryByText(/Total:/i)).toBeDefined();
+    });
+    
+    test('Expand button can close the carrito as well, quesadillas wey', () => {
+        const expandButton = screen.getByRole('button', { name: /Carrito de compra/i });
+        fireEvent.click(expandButton);
+        fireEvent.click(expandButton);
+        
+        expect(screen.queryByText(/Total:/i)).toBeNull();
+    });
 });
